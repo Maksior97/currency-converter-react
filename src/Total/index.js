0deaffbbe31cc61934891b7955031970238b48e6
Maksymilian Chrapkowicz <maksymilian.chrapkowicz@gmail.com>
2023-04-25 10:35:21 +0200

    changed notation of total

--- src/Total/index.js
+++ src/Total/index.js
@@ -3,10 +3,11 @@ import "./style.css";
 const Total = ({total}) => {
     return (
         <span className="window__result">
-            {total !== undefined && 
+            {!!total && 
             <p className="result">
+            Kwota w polskich złotych (PLN):
             <strong className="total">
-                {total.targetSum.toFixed(2)}{total.currency}
+            0.00{total.targetSum.toFixed(2)}
             </strong>
             </p>}
         </span>
